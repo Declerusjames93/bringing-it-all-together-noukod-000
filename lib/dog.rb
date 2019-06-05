b2b2cbaa58fedb2bf8d5b3e4e8a4daf770d4019c
James Declerus <james.declerus@esih.edu>
2019-06-05 13:22:38 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,8 +5,8 @@ class Dog
     @id = id
     @name = name
     @breed = breed
-  end 
-  
+  end
+
   def self.create_table
     sql = <<-SQL
     CREATE TABLE IF NOT EXISTS dogs(
@@ -16,33 +16,33 @@ class Dog
     )
     SQL
     DB[:conn].execute(sql)
-  end 
-  
+  end
+
   def self.drop_table
     DB[:conn].execute("DROP TABLE IF EXISTS dogs")
   end
-  
+
   def save
-      if self.id 
-        self.update 
+      if self.id
+        self.update
       else
         DB[:conn].execute("INSERT INTO dogs (name,breed) VALUES (?,?)",self.name,self.breed)
-        @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0] 
-       
-      end 
+        @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+
+      end
         self
-  end 
-  
+  end
+
   def self.create(name:,breed:)
       dog = self.new(name: name,breed: breed)
       dog.save
-  end 
-  
+  end
+
   def self.find_by_id(id)
     dog = DB[:conn].execute("SELECT * FROM dogs WHERE id =?",id)[0]
     Dog.new(id: dog[0],name: dog[1],breed: dog[2])
-  end 
-  
+  end
+
   def self.find_or_create_by(name:,breed:)
     dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?",name,breed)
     if !dog.empty?
@@ -52,22 +52,22 @@ class Dog
       new_dog = self.create(name: name,breed: breed)
     end
     new_dog
-  end 
-  
+  end
+
   def self.new_from_db(db_row)
     dog = self.new(id: db_row[0],name: db_row[1],breed: db_row[2])
     dog
-  end 
-  
+  end
+
   def self.find_by_name(name)
     dog = DB[:conn].execute("SELECT * FROM dogs WHERE name =?",name)[0]
     dog = self.new_from_db(dog)
-  end 
-  
+  end
+
   def update
     sql = "UPDATE dogs SET name = ?,breed =? WHERE id=?"
     DB[:conn].execute(sql,self.name,self.breed,self.id)
-  end 
+  end
 
 
 
